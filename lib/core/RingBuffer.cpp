1b8ff4f83e45bf06522b5fb5db85429e4e9942cc
Allen Wild <allenwild93@gmail.com>
2018-06-17 16:09:01 -0400

    RingBuffer: remove the template
    To prepare for dynamic memory allocation and resizing at runtime. Clean
    up some coding style while I'm at it.
    
    This effectively reverts
    https://github.com/arduino/ArduinoCore-samd/commit/503a9b39545aeb09e610aec0f639a7f6a7949368

---
+++ lib/core/RingBuffer.cpp
@@ -0,0 +1,96 @@
+/*
+  Copyright (c) 2014 Arduino.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <cstdint>
+#include <cstring>
+
+#include "RingBuffer.h"
+
+RingBuffer::RingBuffer(void)
+{
+  memset(_aucBuffer, 0, SERIAL_BUFFER_SIZE);
+  clear();
+}
+
+void RingBuffer::store_char(uint8_t c)
+{
+  int i = nextIndex(_iHead);
+
+  // if we should be storing the received character into the location
+  // just before the tail (meaning that the head would advance to the
+  // current location of the tail), we're about to overflow the buffer
+  // and so we don't write the character or advance the head.
+  if ( i != _iTail )
+  {
+    _aucBuffer[_iHead] = c;
+    _iHead = i;
+  }
+}
+
+void RingBuffer::clear(void)
+{
+  _iHead = 0;
+  _iTail = 0;
+}
+
+int RingBuffer::read_char(void)
+{
+  if(_iTail == _iHead)
+    return -1;
+
+  uint8_t value = _aucBuffer[_iTail];
+  _iTail = nextIndex(_iTail);
+
+  return value;
+}
+
+int RingBuffer::available(void)
+{
+  int delta = _iHead - _iTail;
+
+  if(delta < 0)
+    return SERIAL_BUFFER_SIZE + delta;
+  else
+    return delta;
+}
+
+int RingBuffer::availableForStore(void)
+{
+  if (_iHead >= _iTail)
+    return SERIAL_BUFFER_SIZE - 1 - _iHead + _iTail;
+  else
+    return _iTail - _iHead - 1;
+}
+
+int RingBuffer::peek(void)
+{
+  if(_iTail == _iHead)
+    return -1;
+
+  return _aucBuffer[_iTail];
+}
+
+int RingBuffer::nextIndex(int index)
+{
+  return (uint32_t)(index + 1) % SERIAL_BUFFER_SIZE;
+}
+
+bool RingBuffer::isFull(void)
+{
+  return (nextIndex(_iHead) == _iTail);
+}
