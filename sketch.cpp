3f3e10cbb474c2dca25bb931cf54a1d706e818ef
Allen Wild <allenwild93@gmail.com>
2018-06-23 17:57:45 -0400

    neostrip demo: moving rainbow pattern
    The floating point hue_to_color calculations add >5k of soft-float code,
    and each hue_to_color call takes about 37us (450-500ms to update all
    8 pixels)

--- sketch.cpp
+++ sketch.cpp
@@ -4,15 +4,17 @@
 #include "wiring_private.h"
 
 #define AIN_BRIGHTNESS 9
+#define STRIP_LENGTH 8
+
+static void hue_to_color(Color& c, float h);
 
 DigitalOut blue_led(13, 1);        // Blue "stat" LED on pin 13
-
-Neostrip<8> ns(SPI);
+Neostrip<STRIP_LENGTH> ns(SPI);
 
 const int N_COLORS = 6;
 const Color colors[] = { RED, YELLOW, GREEN, CYAN, BLUE, MAGENTA };
 
-void setup()
+void setup(void)
 {
     ns.init();
     ns.clear();
@@ -25,22 +27,24 @@ void setup()
     pinPeripheral(13, PIO_OUTPUT);
     blue_led = 1;
 
+#if 0
     while (!SerialUSB)
         delay(10);
     delay(10);
     SerialUSB.print("NeoPixel Demo!\n");
     //ns.dump_rawcolors(SerialUSB);
+#endif
 
     blue_led = 0;
 }
 
-void loop()
+void loop(void)
 {
-    static const unsigned long looptime_ms = 10;
-    static const size_t colorchange_count = 1000 / looptime_ms;
-    static size_t ci = 0;
-    static size_t loopcount = colorchange_count-1;
+    static const float dh = 360.0f / STRIP_LENGTH;
 
+    static float basehue = 0.0f;
+
+#if 0
     if (!SerialUSB)
     {
         blue_led = 1;
@@ -50,18 +54,59 @@ void loop()
             delay(10);
         blue_led = 0;
     }
+#endif
 
     uint8_t b = analogRead(AIN_BRIGHTNESS);
     ns.set_brightness(b);
     ns.write(false); // don't wait for tranfer complete (long delay soon)
 
-    if (++loopcount == colorchange_count)
+    unsigned long m1, m2;
+    m1 = micros();
+    for (size_t i = 0; i < STRIP_LENGTH; i++)
     {
-        SerialUSB.printf("set brightness %u, color %d\n", b, ci);
-        ns.set_all_colors(colors[ci]);
-        ci = (ci + 1) % N_COLORS;
-        loopcount = 0;
+        Color c;
+        //m1 = micros();
+        hue_to_color(c, (dh * i) - basehue);
+        //m2 = micros();
+        ns.set_color(i, c);
+
+        basehue += 0.25f;
+        if (basehue > 360.0f)
+            basehue = 0.0f;
+    }
+    m2 = micros();
+    SerialUSB.printf("%ld\n", m2-m1);
+
+    delay(10);
+}
+
+// Converts HSV to RGB with the given hue, assuming
+// maximum saturation and value
+static void hue_to_color(Color& c, float h)
+{
+    // lots of floating point magic from the internet and scratching my head
+    float r, g, b;
+    if (h > 360.0f)
+        h -= 360.0f;
+    if (h < 0.0f)
+        h += 360.0f;
+    int i = (int)(h / 60.0f);
+    float f = (h / 60.0f) - i;
+    float q = 1.0f - f;
+
+    switch (i % 6)
+    {
+        case 0: r = 1; g = f; b = 0; break;
+        case 1: r = q; g = 1; b = 0; break;
+        case 2: r = 0; g = 1; b = f; break;
+        case 3: r = 0; g = q; b = 1; break;
+        case 4: r = f; g = 0; b = 1; break;
+        case 5: r = 1; g = 0; b = q; break;
+        default: r = 0; g = 0; b = 0; break;
     }
 
-    delay(looptime_ms);
+    // scale to integers and save in the color
+    c.b.red   = (uint8_t)(r * 255.0f);
+    c.b.green = (uint8_t)(g * 255.0f);
+    c.b.blue  = (uint8_t)(b * 255.0f);
 }
