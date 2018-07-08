89d0eea3465ae32b8eec3a3581a9af6d016cb3f3
Allen Wild <allenwild93@gmail.com>
2018-07-07 22:45:41 -0400

    neostrip demo: add in the MPR121 keypad
    Press the keypad buttons to enable or mask out LEDs. Click the RPG to
    toggle whether LEDs are default on or off (instead of changing the
    animation direction).

--- sketch.cpp
+++ sketch.cpp
@@ -1,24 +1,33 @@
 #include "Arduino.h"
 #include "DigitalIO.h"
 #include "Neostrip.h"
+#include "MPR121.h"
+#include "Wire.h"
 #include "wiring_private.h"
 
-#define TIMING_DEBUG 0
-
-#define AIN_BRIGHTNESS 9
 #define STRIP_LENGTH 8
 #define BRIGHTNESS_STEP 2
 
-DigitalOut blue_led(13, 1);        // Blue "stat" LED on pin 13
-Neostrip<STRIP_LENGTH> ns(SPI);
+#define KEYPAD_SDA_PIN 4
+#define KEYPAD_SCL_PIN 3
+#define KEYPAD_IRQ_PIN 2
 
+Neostrip<STRIP_LENGTH> ns(SPI);
+TwoWire i2c(&sercom2, KEYPAD_SDA_PIN, KEYPAD_SCL_PIN);
+MPR121 keypad(i2c);
+
+DigitalOut blue_led(13, HIGH);
+DigitalIn keypad_irq(KEYPAD_IRQ_PIN, INPUT_PULLUP);
 DigitalIn rpg_a(6);
 DigitalIn rpg_b(7);
-DigitalIn rpg_pb(9);
+DigitalIn rpg_pb(9, INPUT_PULLUP);
 
 static volatile uint8_t rpg_state;
 static volatile int brightness = (10 * 255) / 100;
-static volatile int direction = 1;
+static volatile bool invert = false;
+static volatile bool keypad_event = false;
+
+static const int direction = 1;
 
 static inline void read_rpg(void)
 {
@@ -43,7 +52,14 @@ static void rpg_isr(void)
 
 static void rpg_pb_isr(void)
 {
-    direction *= -1;
+    //direction *= -1;
+    invert = !invert;
+}
+
+static void keypad_isr(void)
+{
+    keypad_event = true;
+    blue_led = 1;
 }
 
 static inline uint8_t clamp_brightness(void)
@@ -65,7 +81,6 @@ static inline uint8_t clamp_brightness(void)
 void setup(void)
 {
     blue_led = 1;
-
     read_rpg();
 
     rpg_a.add_interrupt(rpg_isr, CHANGE);
@@ -73,40 +88,36 @@ void setup(void)
     rpg_b.add_interrupt(rpg_isr, CHANGE);
     rpg_b.set_interrupt_filter(true);
 
-    rpg_pb.mode(INPUT_PULLUP);
     rpg_pb.add_interrupt(rpg_pb_isr, FALLING);
+    keypad_irq.add_interrupt(keypad_isr, FALLING);
 
     ns.init();
+    // pin 13 is shared between SCK and the LED, I don't need SCK so
+    // switch back to PORT output
+    pinPeripheral(13, PIO_OUTPUT);
     ns.clear();
     ns.write();
 
-#if 0
-    while (!SerialUSB)
-        delay(10);
-    delay(10);
-    SerialUSB.print("NeoPixel Demo!\n");
-    //ns.dump_rawcolors(SerialUSB);
-#endif
+    // override pinmux settings from variant.cpp
+    i2c.begin();
+    pinPeripheral(KEYPAD_SDA_PIN, PIO_SERCOM_ALT);
+    pinPeripheral(KEYPAD_SCL_PIN, PIO_SERCOM_ALT);
+    keypad.init(false);
 
-    // pin 13 is shared between SCK and the LED, I don't need SCK so
-    // move the mux back to PORT output
-    pinPeripheral(13, PIO_OUTPUT);
     blue_led = 0;
 }
 
 static const int N_STEPS = 256 * 6;
 static Color get_color(int n)
 {
-    Color c = { .i = 0 };
-
     while (n >= N_STEPS)
         n -= N_STEPS;
     while (n < 0)
         n += N_STEPS;
 
+    Color c = { .i = 0 };
     uint8_t stage = n / 256;
     uint8_t step = n % 256;
-
     switch (stage)
     {
         case 0:
@@ -114,38 +125,32 @@ static Color get_color(int n)
             c.b.red = 0xff;
             c.b.green = step;
             break;
-
         case 1:
             // yellow -> green
             c.b.red = 0xff - step;
             c.b.green = 0xff;
             break;
-
         case 2:
             // green -> cyan
             c.b.green = 0xff;
             c.b.blue = step;
             break;
-
         case 3:
             // cyan -> blue
             c.b.green = 0xff - step;
             c.b.blue = 0xff;
             break;
-
         case 4:
             // blue -> magenta
             c.b.red = step;
             c.b.blue = 0xff;
             break;
-
         case 5:
             // magenta -> red
             c.b.blue = 0xff - step;
             c.b.red = 0xff;
             break;
     }
-
     return c;
 }
 
@@ -153,28 +158,26 @@ void loop(void)
 {
     static const int dh = N_STEPS / STRIP_LENGTH;
     static int basehue = 0;
+    static uint16_t keypad_data = 0;
 
-#if 0
-    if (!SerialUSB)
+    if (keypad_event)
     {
-        blue_led = 1;
-        ns.clear();
-        ns.write();
-        while (!SerialUSB)
-            delay(10);
-        blue_led = 0;
+        keypad_data = keypad.readTouchData();
+        blue_led = keypad_event = false;
     }
-#endif
+
+    for (unsigned i = 0; i < 8; i++)
+    {
+        if (( invert && ((keypad_data & (1<<i)) == 0)) ||
+            (!invert && ((keypad_data & (1<<i)) != 0)))
+            ns.set_color(i, BLACK);
+    }
 
     ns.set_brightness(clamp_brightness());
     //SerialUSB.printf("brightness %d, rpg %u\n", brightness, rpg_state);
     ns.write(false); // don't wait for tranfer complete (long delay soon)
 
-#if TIMING_DEBUG
-    unsigned long m1, m2;
-    m1 = micros();
-#endif
-    for (int i = 0; i < STRIP_LENGTH; i++)
+    for (unsigned i = 0; i < STRIP_LENGTH; i++)
     {
         ns.set_color(i, get_color((dh * i) - basehue));
 
@@ -184,10 +187,6 @@ void loop(void)
         else if (basehue < 0)
             basehue = N_STEPS-1;
     }
-#if TIMING_DEBUG
-    m2 = micros();
-    SerialUSB.printf("%ld\n", m2-m1);
-#endif
 
     delay(17);
 }
