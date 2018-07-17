dab0160adc3ddee158ebcef811eb286265b3f3aa
Allen Wild <allenwild93@gmail.com>
2018-07-17 00:00:22 -0400

    MCP41XXX: use SW-controlled chip-select
    MSSEN wasn't working right so just do it manually

--- MCP41XXX.h
+++ MCP41XXX.h
@@ -2,6 +2,7 @@
 #define MCP41XXX_H
 
 #include "SPI.h"
+#include "wiring_digital.h"
 
 #define MCP41XXX_DEFAULT_CLOCK 4000000
 #define MCP41XXX_CMD_SETWIPER 0x1100
@@ -10,11 +11,13 @@
 class MCP41XXX
 {
     public:
-        MCP41XXX(SPIClass& _spi, uint32_t clock=MCP41XXX_DEFAULT_CLOCK) :
-            spi(_spi), spi_settings(clock, MSBFIRST, SPI_MODE0) { }
+        MCP41XXX(SPIClass& _spi, uint32_t _cs_pin, uint32_t clock=MCP41XXX_DEFAULT_CLOCK) :
+            spi(_spi), spi_settings(clock, MSBFIRST, SPI_MODE0), cs_pin(_cs_pin) { }
 
         void init(void)
         {
+            pinMode(cs_pin, OUTPUT);
+            digitalWrite(cs_pin, 1);
             spi.begin();
             shutdown();
         }
@@ -25,14 +28,17 @@ class MCP41XXX
     private:
         SPIClass& spi;
         SPISettings spi_settings;
+        uint32_t cs_pin;
 
         void write(uint16_t data)
         {
             union { uint16_t val; struct { uint8_t lsb; uint8_t msb; }; } t;
             t.val = data;
             spi.beginTransaction(spi_settings);
+            digitalWrite(cs_pin, 0);
             spi.transfer(t.msb);
             spi.transfer(t.lsb);
+            digitalWrite(cs_pin, 1);
             spi.endTransaction();
         }
 };
