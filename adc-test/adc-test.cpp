bc38cf0ff8d8ea7d1b035ad8c777ef75f7a7acdb
Allen Wild <allenwild93@gmail.com>
2018-07-19 23:02:49 -0400

    adc test: set all the registers myself, add some averaging
    I could software reset the ADC, but then might need to re-program the
    calibration registers, so I'll leave it on. I hard-set all the registers
    that matter anyway.
    
    The 16-bit oversampling+decimation is cool, but requires 256 samples for
    the full 16 bits, which takes a very long time since the prescaler and
    sample time are maxed out. Multiple decimated shorter samples were also
    more inaccurate than a single longer sample.
    
    As a compromise, this commit averages 8 full-length samples, which takes
    3.3ms and is pretty stable and accurate.

--- adc-test/adc-test.cpp
+++ adc-test/adc-test.cpp
@@ -4,6 +4,7 @@
 #define ADC_PIN A2
 
 #define ASCALE (3.3 / 4096)
+//#define ASCALE (3.3 / 65536)
 
 #define ADC_SYNC() do { } while (ADC->STATUS.bit.SYNCBUSY)
 #define ADC_GETR(_reg) (ADC->_reg.reg)
@@ -27,12 +28,27 @@ static void adc_init(void)
     // disable ADC
     ADC_CLRB(CTRLA, ADC_CTRLA_ENABLE);
 
-    // set 12-bit precision
-    ADC_SETR(CTRLB, (ADC_GETR(CTRLB) & ~ADC_CTRLB_RESSEL_Msk) | ADC_CTRLB_RESSEL_12BIT);
+    // don't reset to avoid clearing the calibration registers
+    //ADC_SETR(CTRLA, ADC_CTRLA_SWRST);
+    //while (ADC->CTRLA.bit.SWRST);
 
-    // set mux position (pin input)
-    ADC_SETR(INPUTCTRL, (ADC_GETR(INPUTCTRL) & ~ADC_INPUTCTRL_MUXPOS_Msk) |
-                        g_APinDescription[ADC_PIN].ulADCChannelNumber);
+    // set 512x prescaler, 12-bit precision
+    ADC_SETR(CTRLB, ADC_CTRLB_PRESCALER_DIV512 | ADC_CTRLB_RESSEL_12BIT);
+
+    // maximum sampling time (more accurate but slower)
+    ADC_SETR(SAMPCTRL, 0x3f);
+
+    // set mux position and gain - pin input and internal ground
+    ADC_SETR(INPUTCTRL, ADC_INPUTCTRL_MUXPOS(g_APinDescription[ADC_PIN].ulADCChannelNumber) |
+                        ADC_INPUTCTRL_MUXNEG_GND |
+                        ADC_INPUTCTRL_GAIN_DIV2);
+
+    // set internal 1/2 VDDANA reference (3.3/2 = 1.65v)
+    ADC_SETR(REFCTRL, ADC_REFCTRL_REFSEL_INTVCC1);
+
+    // average control
+    ADC_SETR(AVGCTRL, ADC_AVGCTRL_SAMPLENUM_8 |
+                      ADC_AVGCTRL_ADJRES(0x3));
 
     // enable ADC
     ADC_SETB(CTRLA, ADC_CTRLA_ENABLE);
@@ -48,14 +64,15 @@ static uint32_t adc_read(void)
 {
     ADC_SETB(SWTRIG, ADC_SWTRIG_START);
     PORT->Group[0].OUTSET.reg = 1<<2; // fast-set pin 14 PA02
-    while (ADC->INTFLAG.bit.RESRDY == 0);
+    while (!ADC->INTFLAG.bit.RESRDY);
     PORT->Group[0].OUTCLR.reg = 1<<2; // fast-clear pin 14 PA02
-    return ADC->RESULT.reg;
+    return ADC_GETR(RESULT);
 }
 
 void setup(void)
 {
     pinMode(14, OUTPUT);
+    pinPeripheral(ADC_PIN, PIO_ANALOG);
     adc_init();
 
     Serial1.begin(115200);
