202ff311f0246309369de0e53fd076a619ef7cb2
Allen Wild <allenwild93@gmail.com>
2018-07-19 20:40:36 -0400

    low-level ADC test sketch
    Advanced ADC features without wiring_analog.c

--- sketch.cpp
+++ sketch.cpp
@@ -1,58 +1,84 @@
 #include "Arduino.h"
-#include "DigitalIO.h"
-#include "Timeout.h"
+#include "wiring_private.h"
 
-#include <stdlib.h> // for strtoul()
+#define ADC_PIN A2
 
-DigitalOut status_led(13);
-DigitalIn button(9);
+#define ASCALE (3.3 / 4096)
 
-static void timeout_isr(void)
+#define ADC_SYNC() do { } while (ADC->STATUS.bit.SYNCBUSY)
+#define ADC_GETR(_reg) (ADC->_reg.reg)
+#define ADC_SETR(_reg, _val) do { ADC->_reg.reg = (_val); ADC_SYNC(); } while(0)
+#define ADC_SETB(_reg, _val) do { ADC->_reg.reg |= (_val); ADC_SYNC(); } while(0)
+#define ADC_CLRB(_reg, _val) do { ADC->_reg.reg &= ~(_val); ADC_SYNC(); } while(0)
+
+static void adc_init(void)
 {
-    PORT->Group[0].OUTCLR.reg = 1 << 18; // fast clear pin 10 PA18
+    // SystemInit() handles setting the ADC calibration values from NVM
+    // init() (startup.c) configures the ADC with
+    //   * appropriate GCLK settings
+    //   * 512X clock prescaler
+    //   * 10-bit resolution
+    //   * max sampling time (SAMPCTRL = 0x3f)
+    //   * internal GND as negative input
+    //   * 1 sample and no adjust (AVGCTRL)
+    //   * calls analogReference(AR_DEFAULT)
+    //     - INTVCC1 (1/2 VDDANA = 1.65V), GAIN=DIV2
+
+    // disable ADC
+    ADC_CLRB(CTRLA, ADC_CTRLA_ENABLE);
+
+    // set 12-bit precision
+    ADC_SETR(CTRLB, (ADC_GETR(CTRLB) & ~ADC_CTRLB_RESSEL_Msk) | ADC_CTRLB_RESSEL_12BIT);
+
+    // set mux position (pin input)
+    ADC_SETR(INPUTCTRL, (ADC_GETR(INPUTCTRL) & ~ADC_INPUTCTRL_MUXPOS_Msk) |
+                        g_APinDescription[ADC_PIN].ulADCChannelNumber);
+
+    // enable ADC
+    ADC_SETB(CTRLA, ADC_CTRLA_ENABLE);
+
+    // perform and discard one conversion
+    ADC_SETB(SWTRIG, ADC_SWTRIG_START);
+    while (ADC->INTFLAG.bit.RESRDY == 0);
+    ADC->INTFLAG.reg = ADC_INTFLAG_RESRDY;
+    ADC_SYNC();
 }
 
-static void button_isr(void)
+static uint32_t adc_read(void)
 {
-    PORT->Group[0].OUTSET.reg = 1 << 18; // fast set pin 10 PA18
-    timeout_start();
+    ADC_SETB(SWTRIG, ADC_SWTRIG_START);
+    PORT->Group[0].OUTSET.reg = 1<<2; // fast-set pin 14 PA02
+    while (ADC->INTFLAG.bit.RESRDY == 0);
+    PORT->Group[0].OUTCLR.reg = 1<<2; // fast-clear pin 14 PA02
+    return ADC->RESULT.reg;
 }
 
 void setup(void)
 {
-    status_led = 1;
-    pinMode(10, OUTPUT);
-    digitalWrite(10, 0);
-    timeout_init();
-    timeout_set_callback(&timeout_isr);
+    pinMode(14, OUTPUT);
+    adc_init();
 
-    button.mode(INPUT);
-    button.add_interrupt(button_isr, FALLING);
-    status_led = 0;
+    Serial1.begin(115200);
+    Serial1.print("SAMD21 ADC\r\n");
 
     SerialUSB.begin(115200);
-    while (!SerialUSB); // wait for USB host to open the port
-    SerialUSB.print("SAMD21 TC Timer Test\r\n");
-    button_isr();
+    //while (!SerialUSB); // wait for USB host to open the port
+    SerialUSB.print("SAMD21 ADC Test\r\n");
 }
 
 void loop(void)
 {
-    SerialUSB.write("> ");
-    char cmdbuf[16];
-    size_t cmdlen = SerialUSB.readLine(cmdbuf, sizeof(cmdbuf), true);
-    if (cmdlen)
-    {
-        //SerialUSB.printf("count=%u buf='%s'\r\n", cmdlen, cmdbuf);
-        uint32_t val = strtoul(cmdbuf, NULL, 0);
-        if (val)
-        {
-            timeout_set_us(val);
-            uint16_t cc = TC5->COUNT16.CC[0].reg;
-            uint32_t prescaler = TC5->COUNT16.CTRLA.bit.PRESCALER;
-            SerialUSB.printf("%luus, prescaler=%lu, count=%u\r\n",
-                             val, prescaler, cc);
-            button_isr();
-        }
-    }
+    uint32_t value = adc_read();
+    double scaled = value * ASCALE;
+
+    Serial1.printf("%04lx = ", value);
+    Serial1.print(scaled, 3);
+    Serial1.print("\n");
+
+    SerialUSB.printf("%04lx = ", value);
+    SerialUSB.print(scaled, 3);
+    SerialUSB.print("\n");
+    //SerialUSB.printf("%04lx = %f\n", value, scaled);
+
+    delay(1000);
 }
