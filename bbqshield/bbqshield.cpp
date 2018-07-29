f65f7e927e89e0a81c3a0f9ac0377b8a3b3ff0ce
Allen Wild <allenwild93@gmail.com>
2018-07-29 13:05:54 -0400

    bbqshield: rewrite with fading logic; comments and cleanup
    The problem with randomly fluctuating each pixel is that they tend to
    stay roughly the same color over time (since the random drift is
    uniformly centered around zero)
    
    Instead, randomize the whole strip twice to get a starting and ending
    color for each pixel and fade smoothly between those two endpoints.
    
    With this we have ~100Hz updates of the strip and a single fade takes
    about 2.5 seconds to go through all 257 steps. There's definitely some
    color compression going on between the fading and the cie1931
    correction, but it's not noticeable. Also not noticeable is the extra
    250us it takes between fades to come up with the next fade endpoints.
    I could hook up a timer for precise timing, but that's too much effort
    to be worth it and the tiny irregularity can't be seen.

--- bbqshield/bbqshield.cpp
+++ bbqshield/bbqshield.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * BBQ Shield LED Control
+ * BBQ Leona Shield LED Control
  *
  * Copyright (C) 2018 Allen Wild <allenwild93@gmail.com>
  *
@@ -17,39 +17,51 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  ******************************************************************************/
 
-#define DEBUG_PORT 0
-#define DEBUG_PIN  17
-
-#include <stdlib.h>
 #include "Arduino.h"
 #include "DigitalIO.h"
 #include "wiring_private.h"
-#include "debug_macros.h"
-
 #include "Neostrip.h"
 #include "gradient.h"
 
-#define RANDOM_SEED  0xa1e600
-#define STRIP_LENGTH 32
-#define BRIGHTNESS   ((30 * 255) / 100)
+// RNG
+#include <stdlib.h>
+#define RANDOM       lrand48
+#define SRAND        srand48
+#define RANDOM_SEED  0xAAC0FFEE
 
-DigitalOut blue_led(13);
-Neostrip<STRIP_LENGTH> ns(SPI, BRIGHTNESS);
+// debug pin PA17 - pin 13, also the blue LED
+#define DEBUG_PORT 0
+#define DEBUG_PIN  17
+#include "debug_macros.h"
 
-DigitalIn button1(7, INPUT_PULLUP);
-DigitalIn button2(8, INPUT_PULLUP);
+// how many pixels in the strip?
+#define STRIP_LENGTH    90
 
-static uint8_t colors[GRADIENT_SIZE];
+// starting brightness and adjustment step
+#define DEF_BRIGHTNESS  225
+#define BRIGHTNESS_STEP 15
 
-static volatile int brightness = BRIGHTNESS;
-static void brightness_up(void)
-{
-    brightness += 15;
-}
-static void brightness_down(void)
-{
-    brightness -= 15;
-}
+// number of steps to fade between states, and delay between each
+// FADE_STEPS should be 2^n+1 to make dividing by FADE_STEPS-1 fast
+#define FADE_STEPS      257
+#define STEP_DELAY_MS   10
+
+// static objects
+Neostrip<STRIP_LENGTH> ns(SPI, DEF_BRIGHTNESS);
+DigitalIn pb_bright_up(7, INPUT_PULLUP);
+DigitalIn pb_bright_down(8, INPUT_PULLUP);
+
+// fade endpoint arrays (values are indicies to the gradient)
+static uint8_t colors1[STRIP_LENGTH];
+static uint8_t colors2[STRIP_LENGTH];
+
+// brightness control variables
+static volatile int brightness = DEF_BRIGHTNESS;
+static volatile bool brightness_update = true;
+
+// brightness control ISRs
+static void brightness_up(void)   { brightness += BRIGHTNESS_STEP; brightness_update = true; }
+static void brightness_down(void) { brightness -= BRIGHTNESS_STEP; brightness_update = true; }
 static inline uint8_t clamp_brightness(void)
 {
     __disable_irq();
@@ -58,64 +70,83 @@ static inline uint8_t clamp_brightness(void)
     {
         b = brightness = 0;
     }
-    if (b > 255)
+    else if (b > 255)
     {
         b = brightness = 255;
     }
+    brightness_update = false;
     __enable_irq();
     return (uint8_t)b;
 }
 
-static inline uint8_t gradient_clamp(int index)
-{
-    if (index < 0)
-        return 0;
-    if (index > GRADIENT_SIZE-1)
-        return GRADIENT_SIZE-1;
-    return index;
-}
-
-static void set_colors(void)
-{
-    for (size_t i = 0; i < STRIP_LENGTH; i++)
-        ns.set_color(i, gradient_data[colors[i]]);
-}
-
 void setup(void)
 {
-    blue_led = 1;
-    button1.add_interrupt(brightness_up, FALLING);
-    button2.add_interrupt(brightness_down, FALLING);
-    srand(RANDOM_SEED);
-
-    ns.init();
+    // init debug pin, RNG, and brightness button IRQs
     DBGINIT();
+    DBGHIGH();
+    SRAND(RANDOM_SEED);
+    pb_bright_up.add_interrupt(brightness_up, FALLING);
+    pb_bright_down.add_interrupt(brightness_down, FALLING);
+
+    // init the SPI and move pin 13 back to GPIO for debug rather than SCLK
+    SPI.begin();
     pinPeripheral(13, PIO_OUTPUT);
 
+    // init and clear neostrip
+    ns.init(false);
+    ns.write(false);
+
+    // set the fade endpoints and load the first frame into the strip's buffer
     for (size_t i = 0; i < STRIP_LENGTH; i++)
     {
-        colors[i] = random() % GRADIENT_SIZE;
+        colors1[i] = RANDOM() % GRADIENT_SIZE;
+        colors2[i] = RANDOM() % GRADIENT_SIZE;
+        ns.set_color(i, gradient_data[colors1[i]]);
     }
-    set_colors();
-    ns.write();
 
-    blue_led = 0;
+    ns.wait_for_complete();
+    DBGLOW();
 }
 
 void loop(void)
 {
-    ns.set_brightness(clamp_brightness());
-    ns.write(false); // don't wait for tranfer complete (long delay soon)
+    static uint8_t *cstart = colors1;
+    static uint8_t *cstop  = colors2;
 
-    DBGHIGH();
-    for (size_t i = 0; i < STRIP_LENGTH; i++)
-    {
-#define RANDOM_RANGE 41
-        int delta = (random() % RANDOM_RANGE) - (RANDOM_RANGE / 2);
-        colors[i] = gradient_clamp(colors[i] + delta);
-    }
-    set_colors();
     DBGLOW();
+    for (int i = 0; i < FADE_STEPS; i++)
+    {
+        // write current frame
+        if (brightness_update)
+            ns.set_brightness(clamp_brightness());
+        ns.write(false);
 
-    delay(100);
+        // prepare the next frame with a linear interpolation
+        for (int j = 0; j < STRIP_LENGTH; j++)
+        {
+            uint8_t gc = cstart[j] + ((i * (cstop[j] - cstart[j])) / (FADE_STEPS-1));
+            ns.set_color(j, gradient_data[gc]);
+        }
+
+        // wait for next frame. The last frame of the fade won't actually get
+        // displayed until the next time through loop(), where it's the preloaded
+        // first frame of the next fade.
+        delay(STEP_DELAY_MS);
+    }
+    DBGHIGH();
+
+    // randomize the starting points and then flip buffers
+    for (int i = 0; i < STRIP_LENGTH; i++)
+        cstart[i] = RANDOM() % GRADIENT_SIZE;
+
+    if (cstart == colors1)
+    {
+        cstart = colors2;
+        cstop = colors1;
+    }
+    else
+    {
+        cstart = colors1;
+        cstop = colors2;
+    }
 }
